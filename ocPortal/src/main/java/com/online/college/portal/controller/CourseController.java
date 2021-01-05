7bbbdcd23595aba4153956c2a331f68fea47f3a1
liu1fan <962713793@qq.com>
2021-01-05 16:36:21 +0800

    课程详情页——02课程信息、章节、教师、推荐课程

---
+++ ocPortal/src/main/java/com/online/college/portal/controller/CourseController.java
@@ -0,0 +1,76 @@
+package com.online.college.portal.controller;
+
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.online.college.common.storage.QiniuStorage;
+import com.online.college.core.auth.domain.AuthUser;
+import com.online.college.core.auth.service.IAuthUserService;
+import com.online.college.core.course.domain.Course;
+import com.online.college.core.course.domain.CourseQueryDto;
+import com.online.college.core.course.service.ICourseService;
+import com.online.college.portal.business.ICourseBusiness;
+import com.online.college.portal.vo.CourseSectionVO;
+
+/**
+ * 课程详情信息
+ */
+@Controller
+@RequestMapping("/course")
+public class CourseController {
+	
+	@Autowired
+	private ICourseBusiness courseBusiness;
+	
+	@Autowired
+	private ICourseService courseService;
+	
+	@Autowired
+	private IAuthUserService authUserService;
+	
+	
+	/**
+	 * 课程章节页面
+	 * @return
+	 */
+	@RequestMapping("/learn/{courseId}")
+	public ModelAndView learn(@PathVariable Long courseId){
+		if(null == courseId)
+			return new ModelAndView("error/404"); 
+		
+		//获取课程
+		Course course = courseService.getById(courseId);
+		if(null == course)
+			return new ModelAndView("error/404"); 
+		
+		//获取课程章节
+		ModelAndView mv = new ModelAndView("learn");
+		List<CourseSectionVO> chaptSections = this.courseBusiness.queryCourseSection(courseId);
+		mv.addObject("course", course);
+		mv.addObject("chaptSections", chaptSections);
+		
+		//获取讲师
+		AuthUser courseTeacher = this.authUserService.getByUsername(course.getUsername());
+		if(StringUtils.isNotEmpty(courseTeacher.getHeader())){
+			courseTeacher.setHeader(QiniuStorage.getUrl(courseTeacher.getHeader()));
+		}
+		mv.addObject("courseTeacher", courseTeacher);
+		
+		//获取推荐课程
+		CourseQueryDto queryEntity = new CourseQueryDto();
+		queryEntity.descSortField("weight");
+		queryEntity.setCount(5);//5门推荐课程
+		queryEntity.setSubClassify(course.getSubClassify());
+		List<Course> recomdCourseList = this.courseService.queryList(queryEntity);
+		mv.addObject("recomdCourseList", recomdCourseList);
+		
+		return mv;
+	}
+	
+}
