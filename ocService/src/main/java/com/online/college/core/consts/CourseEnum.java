f0253c05779312fb5a0dabdf2915410dc13f46cd
liu1fan <962713793@qq.com>
2021-01-08 16:45:44 +0800

    课程收藏，用关注

--- ocService/src/main/java/com/online/college/core/consts/CourseEnum.java
+++ ocService/src/main/java/com/online/college/core/consts/CourseEnum.java
@@ -5,13 +5,15 @@ package com.online.college.core.consts;
  */
 public enum CourseEnum {
 
-	FREE(1), //免费
-	FREE_NOT(0), //收费
+	FREE(1),							//免费
+	FREE_NOT(0), 						//收费
 	
-	ONSALE(1), //上架
-	ONSALE_NOT(0); //下架
-	
-	
+	ONSALE(1), 							//上架
+	ONSALE_NOT(0), 						//下架
+
+	COLLECTION_CLASSIFY_COURSE(1);		//课程收藏
+
+
 	private Integer value;
 	private CourseEnum(Integer value) {
 		this.value = value;
---
+++ ocService/src/main/java/com/online/college/core/user/service/impl/UserFollowsServiceImpl.java
@@ -0,0 +1,67 @@
+package com.online.college.core.user.service.impl;
+
+import com.online.college.common.page.TailPage;
+import com.online.college.core.user.dao.UserFollowsDao;
+import com.online.college.core.user.domain.UserFollowStudyRecord;
+import com.online.college.core.user.domain.UserFollows;
+import com.online.college.core.user.service.IUserFollowsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class UserFollowsServiceImpl implements IUserFollowsService {
+
+	@Autowired
+	private UserFollowsDao entityDao;
+
+	public UserFollows getById(Long id){
+		return entityDao.getById(id);
+	}
+
+	public List<UserFollows> queryAll(UserFollows queryEntity){
+		return entityDao.queryAll(queryEntity);
+	}
+
+	public TailPage<UserFollows> queryPage(UserFollows queryEntity , TailPage<UserFollows> page){
+		Integer itemsTotalCount = entityDao.getTotalItemsCount(queryEntity);
+		List<UserFollows> items = entityDao.queryPage(queryEntity,page);
+		page.setItemsTotalCount(itemsTotalCount);
+		page.setItems(items);
+		return page;
+	}
+	
+	public TailPage<UserFollowStudyRecord> queryUserFollowStudyRecordPage(UserFollowStudyRecord queryEntity , TailPage<UserFollowStudyRecord> page){
+		Integer itemsTotalCount = entityDao.getFollowStudyRecordCount(queryEntity);
+		List<UserFollowStudyRecord> items = entityDao.queryFollowStudyRecord(queryEntity,page);
+		page.setItemsTotalCount(itemsTotalCount);
+		page.setItems(items);
+		return page;
+	}
+
+	public void createSelectivity(UserFollows entity){
+		entityDao.createSelectivity(entity);
+	}
+
+	public void update(UserFollows entity){
+		entityDao.update(entity);
+	}
+
+	public void updateSelectivity(UserFollows entity){
+		entityDao.updateSelectivity(entity);
+	}
+
+	public void delete(UserFollows entity){
+		entityDao.delete(entity);
+	}
+
+	public void deleteLogic(UserFollows entity){
+		entityDao.deleteLogic(entity);
+	}
+
+
+
+}
+
